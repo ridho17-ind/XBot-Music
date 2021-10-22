56997a9eedcc3f98e26d0e885b58d218c130bfc8
levina <82658782+levina-lab@users.noreply.github.com>
2021-10-22 22:02:46 +0700

    none

--- handlers/play.py
+++ handlers/play.py
@@ -643,7 +643,7 @@ async def play(_, message: Message):
         for i in message.command[1:]:
             query += " " + str(i)
         print(query)
-        ydl_opts = {"format": "bestaudio/best"}
+        ydl_opts = {"format": "bestaudio[ext=m4a]"}
 
         try:
             results = YoutubeSearch(query, max_results=5).to_dict()
